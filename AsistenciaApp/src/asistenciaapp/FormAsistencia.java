64fe3fef98c27a194fdd81e65b937248a9f50591
DIGP1 <Diegopimentel00124@gmail.com>
2022-11-29 21:43:36 -0600

    Sen agrega el formulario de asistencia por evento

--- AsistenciaApp/src/asistenciaapp/FormAsistencia.java
+++ AsistenciaApp/src/asistenciaapp/FormAsistencia.java
@@ -72,6 +72,7 @@ public class FormAsistencia extends javax.swing.JFrame {
         btnBuscarAsistencia = new javax.swing.JToggleButton();
         txtFecha = new javax.swing.JLabel();
         jcEvento = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
@@ -273,6 +274,13 @@ public class FormAsistencia extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jButton1.setText("Abrir form de asistencia por evento");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -282,7 +290,9 @@ public class FormAsistencia extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 552, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(27, 27, 27))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(41, 41, 41)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -299,11 +309,13 @@ public class FormAsistencia extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(51, 51, 51)
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel1)))
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1)
+                            .addComponent(jLabel2))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -392,6 +404,13 @@ public class FormAsistencia extends javax.swing.JFrame {
         cn.insertarAsistencia(asis);
         cargardatos();
     }//GEN-LAST:event_btnGuardarAsistenciaActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        FormEsta esta = new FormEsta();
+        esta.setVisible(true);
+        esta.setLocationRelativeTo(null);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     
      public void buscar(String nombres){
@@ -566,6 +585,7 @@ public class FormAsistencia extends javax.swing.JFrame {
     private javax.swing.JButton btneliminar;
     private javax.swing.JTable dtasistencia;
     private javax.swing.ButtonGroup grupoSeleccion;
+    private javax.swing.JButton jButton1;
     private javax.swing.JRadioButton jButtonAsistio;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
